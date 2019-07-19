22bc551c8eb1b33fabe686cd2e751a084e85ec76
NapoleonDLP <napoleon.dlp@gmail.com>
2019-07-19 00:14:28 -0700

    serve static html

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const app = express();
 const port = 4578;
 const db = require('./db.js');
 
-app.get('/', (req, res) => res.send(`Server says, 'Hello world!', from port ${port} `));
+// app.get('/', (req, res) => res.send(`Server says, 'Hello world!', from port ${port} `));
+app.use(express.static('public'));
+
+
+//TODO: build endpoints for app, Must have a GET, POST, DELETE
 
 app.listen(port, () => console.log(`listening to port ${port}`));
